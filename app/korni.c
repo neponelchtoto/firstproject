24675f93aad0d4ce03a97b63d4b3b9921276f1e8
neponelchtoto <124793254+neponelchtoto@users.noreply.github.com>
2023-03-24 16:11:39 +0300

    Update korni.c

--- app/korni.c
+++ app/korni.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include "korni.h"
+#include "myfunc.h"
 
-int korni(double a, double b, double c)
-{
-    double D, x;
-    D = b * b - 4 * a * c;
+int val;
 
-    if (D > 0)
-    {
-        x = (-b + sqrt(D)) / (2 * a);
-        printf("X1 = %f \n", x);
-        x = (-b - sqrt(D)) / (2 * a);
-        printf("X2 = %f \n", x);
-    }
-    else if (D < 0)
-    {
-        printf("No Result \n");
-    }
-    else
-    {
-        x = -b / 2 * a;
-        printf("x = %f \n", x);
-    }
+int korni(float a, float b, float c, float* x1, float* x2, int* flag) {
+  float d;
+  *flag = 0;
+
+  d = b*b - 4*a*c;
+
+  if (a == 0) {
+      if (b != 0) {
+        *x1 = *x2 = -c/b;
+      }
+      else {
+        *flag = 1;
+      }
     return 0;
+  }
+
+  if (d>0) {
+      *x1 = (-b + sqrt(d)) / (2*a);
+      *x2 = (-b - sqrt(d)) / (2*a);
+  }
+  else
+    if (d == 0) {
+        *x1 = -b/(2*a);
+        *x2 = -b/(2*a);
+     }
+    else
+        *flag = 1;
+  return 0;
 }
+
