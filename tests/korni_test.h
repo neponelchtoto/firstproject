425b79fc802d1ecfb94b37389e6f2e6817926283
neponelchtoto <124793254+neponelchtoto@users.noreply.github.com>
2023-03-24 16:46:26 +0300

    Update korni_test.h

--- tests/korni_test.h
+++ tests/korni_test.h
@@ -11,40 +11,46 @@ extern "C" {
 #include "korni.h"
 }
 
-TEST(korniTest, num0) {
-    ASSERT_EQ(korni(0), 0);
-    ASSERT_EQ(korni(1), 1);
-    ASSERT_EQ(korni(2), 2);
+float x1, x2;
+int flag;
+
+
+TEST(Discr, num1) {
+    korni(1, -3, 4, &x1, &x2, &flag);
+    ASSERT_EQ(1, flag);    
 }
 
-TEST(fibonachiTest, negative) {
-    ASSERT_EQ(korni(-1), 0);
+TEST(Discr, num2) {
+    korni(1, 2, 1, &x1, &x2, &flag);
+    ASSERT_FLOAT_EQ(-1, x1);
+    ASSERT_FLOAT_EQ(-1, x2);
+    ASSERT_EQ(0, flag);
 }
 
-TEST(fibonachiTest, inputFile) {
-    val = 2;
-    char *filename = (char *)malloc(sizeof(char) * 1024);
-    sprintf(filename, "%s/input321.txt", INPUTDIR);
-
-    int fd = open(filename, O_RDONLY);
-    free(filename);
-    if (fd < 0) {
-        ASSERT_EQ(errno, 0);
-    }
-
-    char *buf = (char *)malloc(sizeof(char) * 512);
-    int readcount = read(fd, buf, 512);
-    ASSERT_TRUE(readcount > 0);
-    close(fd);
-
-    int input = 0;
-    int output =0;
-    int ret = sscanf(buf, "%d %d", &input, &output);
-    free(buf);
-    ASSERT_EQ(ret, 2);
-
-    ret = korni(input);
-    ASSERT_EQ(ret, output);
+TEST(Discr, num3) {
+    korni(1, -4, 3, &x1, &x2, &flag);
+    ASSERT_FLOAT_EQ(3, x1);
+    ASSERT_FLOAT_EQ(1, x2);
+    ASSERT_EQ(0, flag);
 }
 
-#endif // KORNI_H
+TEST(Discr, num4) {
+    korni(1, 4.5, 3, &x1, &x2, &flag);
+    ASSERT_FLOAT_EQ(-0.813859, x1);
+    ASSERT_FLOAT_EQ(-3.686141, x2);
+    ASSERT_EQ(0, flag);
+}
+
+TEST(Koefzero, num5) {
+    qvadr(0, 4, 3, &x1, &x2, &flag);
+    ASSERT_EQ(flag, 1);
+}
+
+TEST(Koefzero, num6) {
+    qvadr(1, 0, -9, &x1, &x2, &flag);
+    ASSERT_FLOAT_EQ(3, x1);
+    ASSERT_FLOAT_EQ(-3, x2);
+    ASSERT_EQ(flag, 1);
+}
+
+#endif // QVADR_H
