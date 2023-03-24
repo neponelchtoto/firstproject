5f92f15a05b508c94356120d0f8efcfe8b84155d
neponelchtoto <124793254+neponelchtoto@users.noreply.github.com>
2023-03-24 16:13:24 +0300

    Update korni.h

--- app/korni.h
+++ app/korni.h
@@ -1,7 +1,7 @@
 #ifndef KORNI_H
 #define KORNI_H
 
-int korni(float a, float b, float c);
+int korni(float a, float b, float c, float* x1, float* x2, int* flag);
 
 extern int val;
 #endif // KORNI_H
