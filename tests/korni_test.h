36792f54b3e211761921156e30f4c81ae8cd39b8
neponelchtoto <124793254+neponelchtoto@users.noreply.github.com>
2023-04-07 15:54:06 +0300

    Update korni_test.h

--- tests/korni_test.h
+++ tests/korni_test.h
@@ -1,6 +1,3 @@
-#ifndef KORNI_H
-#define KORNI_H
-
 #include <gtest/gtest.h>
 
 #include <fcntl.h>
@@ -35,22 +32,22 @@ TEST(Discr, num3) {
 }
 
 TEST(Discr, num4) {
-    korni(1, 4.5, 3, &x1, &x2, &flag);
-    ASSERT_FLOAT_EQ(-0.813859, x1);
-    ASSERT_FLOAT_EQ(-3.686141, x2);
+    korni(1, 5, 3, &x1, &x2, &flag);
+    ASSERT_FLOAT_EQ(-0.697224, x1);
+    ASSERT_FLOAT_EQ(-4.30278, x2);
     ASSERT_EQ(0, flag);
 }
 
 TEST(Koefzero, num5) {
     korni(0, 4, 3, &x1, &x2, &flag);
-    ASSERT_EQ(flag, 1);
+    ASSERT_EQ(flag, 0);
 }
 
 TEST(Koefzero, num6) {
     korni(1, 0, -9, &x1, &x2, &flag);
     ASSERT_FLOAT_EQ(3, x1);
     ASSERT_FLOAT_EQ(-3, x2);
-    ASSERT_EQ(flag, 1);
+    ASSERT_EQ(flag, 0);
 }
 
 #endif // QVADR_H
